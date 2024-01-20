b08ef9fafdd842d8f6c6516157738df5ebed3105
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2024-01-20 22:00:52 +0000

    Added prompt questions in index.js and wrote function

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+// const Manager = require("./lib/Manager");
+
+const ManagerData = [ 
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the team manager\'s name?',      
+    },
+    {
+        type: 'number',
+        name: 'id',
+        message: 'What is the team manager\'s id?', 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the team manager\'s email?'
+    },
+    {
+        type: 'number',
+        name: 'officeNumber',
+        message: 'What is the team manager\'s office number'
+    }
+]
+
+function getInfo(){
+    inquirer
+    .prompt(ManagerData)
+    .then((data) =>{
+
+        const Manager1 = new Manager(data.name, data.id, data.email, data.officeNumber)
+        console.log(Manager1)
+    })
+}
+
+getInfo()
+
+
+
+
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
